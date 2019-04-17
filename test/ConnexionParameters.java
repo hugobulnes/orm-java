755e5f03aa69e2405e080d58838152a2b34bb115
Hugo Bulnes <hugobulnes@hotmail.com>
2019-04-16 20:07:39 -0700

    version 2

--- test/ConnexionParameters.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author da-nee
- *
- */
-public final class ConnexionParameters {
-	public static final String url = "jdbc:mysql://localhost:3306/jagdevdb";
-	public static final String user = "root";
-	public static final String password = "Sp@c3C0wLun@r";  
-}
